e62f0cc6c362225a798806db476ba519ea211eff
Marwan Haitham <merohai204@gmail.com>
2024-08-10 13:39:32 +0300

    improved login form using gridpane (will be used in dashboard) fixed back button functionality more styling added (will move to css soon)

--- src/main/java/com/example/demo/Style.java
+++ src/main/java/com/example/demo/Style.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
 public interface Style {
-    String H1 = "-fx-font-weight: bold; -fx-font-size: 32pt; -fx-font-style: italic; -fx-font-family: Arial";
-    String ButtonStyle = "-fx-font-size: 25pt; -fx-font-family: Arial";
+    String H1 = "-fx-font-weight: bold; -fx-font-size: 32pt; -fx-font-style: italic; -fx-font-family: Arial;";
+    String H2 = "-fx-font-size: 26pt; -fx-font-family: Arial;";
+    String H3 = "-fx-font-size: 20pt; -fx-font-family: Arial;";
+    String Warning = "-fx-border-radius: 5pt; -fx-border-color: red; -fx-background-color: #f5cbc9 ;-fx-padding: 10px;-fx-text-fill: red;";
+    String Success = "-fx-border-radius: 5pt; -fx-border-color: lime; -fx-background-color: #c9f5ca ;-fx-padding: 10px;-fx-text-fill: green;";
+
+
+    String ButtonStyle = "-fx-border-radius: 5pt;-fx-font-size: 25pt; -fx-font-family: Arial;";
     String BGColor = "-fx-background-color: #baf5ee;";
-    int bwidth = 240;
-    int blength= 120;
+    int bwidth = 280;
+    int blength= 140;
 }
--- src/main/java/com/example/demo/main.java
+++ src/main/java/com/example/demo/main.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,19 +15,19 @@ import java.io.IOException;
 
 public class main extends Application implements Style {
     private Stage window;
-    private Scene PreviousScene;
     private String UserType;
     public void start(Stage stage) throws IOException {
         window = stage;
         window.setTitle("Healthcare System v1.0");
+        int[] Resolution = {1920,1080};
         // Layouts
-        StackPane menuPane = new StackPane();
+        BorderPane menuPane = new BorderPane();
         BorderPane loginOPane = new BorderPane();
         BorderPane loginFPane = new BorderPane();
         // Scenes
-        Scene menu = new Scene(menuPane, 1920, 1080);
-        Scene LoginOptions = new Scene(loginOPane,1920,1080);
-        Scene LoginForm = new Scene(loginFPane,1920,1080);
+        Scene Menu = new Scene(menuPane,Resolution[0],Resolution[1]);
+        Scene LoginOptions = new Scene(loginOPane,Resolution[0],Resolution[1]);
+        Scene LoginForm = new Scene(loginFPane,Resolution[0],Resolution[1]);
 
         // Menu Page
         {
@@ -39,17 +40,14 @@ public class main extends Application implements Style {
                 button[i].setMinSize(bwidth, blength);
                 button[i].setStyle(ButtonStyle);
             }
-            button[0].setOnAction(e -> {
-                PreviousScene = window.getScene();
-                stage.setScene(LoginOptions);
-            });
+            button[0].setOnAction(e -> stage.setScene(LoginOptions));
             button[2].setOnAction(e -> stage.close());
             VBox mlist = new VBox(50);
             mlist.setStyle(BGColor);
             mlist.setAlignment(Pos.CENTER);
             mlist.getChildren().add(label);
             mlist.getChildren().addAll(button);
-            menuPane.getChildren().addAll(mlist);
+            menuPane.setCenter(mlist);
         }
         // Login Options Page
         {
@@ -62,60 +60,78 @@ public class main extends Application implements Style {
             }
             button[0].setOnAction(e -> {
                 setUserType("Admin");
-                PreviousScene = window.getScene();
                 window.setScene(LoginForm);
             });
             button[1].setOnAction(e -> {
                 setUserType("Doctor");
-                PreviousScene = window.getScene();
                 window.setScene(LoginForm);
             });
-            Button back = BackButton();
+            Button backButton = BackButton();
+            backButton.setOnAction(e-> window.setScene(Menu));
             VBox blist = new VBox(50);
             loginOPane.setStyle(BGColor);
             blist.setAlignment(Pos.CENTER);
             blist.getChildren().addAll(button);
             loginOPane.setCenter(blist);
-            loginOPane.setBottom(back);
+            loginOPane.setBottom(backButton);
         }
         // Login Form Page
         {
-            final String CORRECT_USERNAME = "admin";
-            final String CORRECT_PASSWORD = "pass123";
+            String CORRECT_USERNAME = "admin";
+            String CORRECT_PASSWORD = "pass123";
             Label usernameLabel = new Label("Username:");
-            usernameLabel.setStyle(H1);
+            usernameLabel.setStyle(H2);
             Label passwordLabel = new Label("Password:");
-            passwordLabel.setStyle(H1);
+            passwordLabel.setStyle(H2);
             TextField usernameField = new TextField();
-            usernameField.setMaxSize(320,200);
+            usernameField.setStyle(H3);
+            usernameField.setPrefSize(350,200);
+            usernameField.setPromptText("Username");
             PasswordField passwordField = new PasswordField();
-            passwordField.setMaxSize(320,200);
+            passwordField.setPrefSize(350,200);
+            passwordField.setPromptText("Password");
+            passwordField.setStyle(H3);
             Label resultLabel = new Label();
-            resultLabel.setStyle(H1);
+            resultLabel.setMaxSize(400,120);
             Button loginButton = new Button("Login");
+            loginButton.setMaxSize(200,120);
             Button backButton = BackButton();
+            backButton.setOnAction(e-> window.setScene(LoginOptions));
             loginButton.setStyle(ButtonStyle);
             loginButton.setOnAction(e -> {
                 String enteredUsername = usernameField.getText();
                 String enteredPassword = passwordField.getText();
                 if (enteredUsername.equals(CORRECT_USERNAME) && enteredPassword.equals(CORRECT_PASSWORD)) {
-                    resultLabel.setText("Login successful!");
+                    resultLabel.setText("Successful Login!");
+                    resultLabel.setStyle(Success + H2);
                 } else {
-                    resultLabel.setText("Login failed. Please check your credentials." + getUserType());
+                    resultLabel.setText("Incorrect Credentials!");
+                    resultLabel.setStyle(Warning + H2);
                 }
             });
-            VBox form = new VBox(10);
-            form.getChildren().addAll(usernameLabel, usernameField, passwordLabel, passwordField, loginButton, resultLabel);
-            form.setAlignment(Pos.CENTER);
-            form.setPrefSize(400,400);
+
+            GridPane form = new GridPane();
+            form.add(usernameLabel, 0, 0);
+            form.add(usernameField, 1, 0);
+            form.add(passwordLabel, 0, 1);
+            form.add(passwordField, 1, 1);
+            form.add(resultLabel, 1, 2);
+            form.add(loginButton, 0, 2);
+            form.setHgap(20);
+            form.setVgap(25);
+            form.setPadding(new Insets(75,40,75,40));
+            form.setStyle("-fx-background-color: #eeeeee");
+            form.setMaxSize(680,330);
+
             loginFPane.setCenter(form);
+            loginFPane.setStyle(BGColor);
             loginFPane.setBottom(backButton);
         }
         // DashBoard
         {
             String[] labels = new String[]{"Physician Login","Admin Login"};
         }
-        window.setScene(menu);
+        window.setScene(Menu);
         window.show();
     }
     public static void main(String[] args) {
@@ -125,11 +141,6 @@ public class main extends Application implements Style {
         Button backbutton = new Button("Back");
         backbutton.setMinSize(bwidth,blength);
         backbutton.setStyle(ButtonStyle);
-        backbutton.setOnAction(e -> {
-            Scene temp = window.getScene();
-            window.setScene(PreviousScene);
-            PreviousScene = temp;
-        });
         return backbutton;
     }
     public String getUserType() {
