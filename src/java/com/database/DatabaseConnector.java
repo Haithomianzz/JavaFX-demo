be8728551be363ae819c46bd92ab5952931b955c
Maro <merohai204@gmail.com>
2024-09-22 13:20:11 +0300

    remove connection string for bima

--- src/java/com/database/DatabaseConnector.java
+++ src/java/com/database/DatabaseConnector.java
@@ -8,9 +8,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
 
-    private final static String CONN_STRING = "jdbc:mysql://4.233.221.225:3306/healthcare";
-    private final static String username = "applicationAdmin";
-    private  static final  String password = "nabnuAdmin";
+
     private static MysqlDataSource dataSource = new MysqlDataSource();
 
     public static void connect() {
